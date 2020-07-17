dcd209f1b259da9d1a98066ddeba6643f6d3f58b
knts0 <shimmer22.0204@gmail.com>
2020-07-17 19:04:06 +0900

    add: schematics files

---
+++ src/schematics/basic/index.ts
@@ -0,0 +1,27 @@
+import {
+    Rule,
+    SchematicContext,
+    // Tree,
+    apply,
+    branchAndMerge,
+    mergeWith,
+    template,
+    url
+  } from '@angular-devkit/schematics';
+  // import { Schema as ClassOptions } from './schema';
+  import { strings } from '@angular-devkit/core';
+  
+  
+  // You don't have to export the function as default. You can also have more than one rule factory
+  // per file.
+  export function basicSchematics(options: any): Rule {
+    return (_, _context: SchematicContext) => {
+      return branchAndMerge(mergeWith(apply(url('./template-files'), [
+          template({
+            ...strings,
+            ...options,
+          }),
+        ]
+      )))};
+  }
+  
---
+++ src/schematics/basic/template-files/__name__/hero.actions.ts
@@ -0,0 +1,38 @@
+import { <%= classify(name) %> } from './<%= name %>';
+
+export module <%= classify(name) %>Action {
+
+  export const LOAD_<%= name.toUpperCase() %> = 'Load_<%= classify(name) %>';
+  export const SELECT_<%= name.toUpperCase() %> = 'Select_<%= classify(name) %>';
+  export const ADD_<%= name.toUpperCase() %> = 'Add_<%= classify(name) %>';
+  export const DELETE_<%= name.toUpperCase() %> = 'Delete_<%= classify(name) %>';
+  export const UPDATE_<%= name.toUpperCase() %> = 'Update_<%= classify(name) %>';
+
+  export class Load {
+    static readonly type = LOAD_<%= name.toUpperCase() %>;
+  }
+
+  export class Select {
+    static readonly type = SELECT_<%= name.toUpperCase() %>;
+
+    constructor(public id: number) {}
+  }
+
+  export class Add {
+    static readonly type = ADD_<%= name.toUpperCase() %>;
+
+    constructor(public payload: <%= classify(name) %>) {}
+  }
+
+  export class Delete {
+    static readonly type = DELETE_<%= name.toUpperCase() %>;
+
+    constructor(public payload: <%= classify(name) %>) {}
+  }
+
+  export class Update {
+    static readonly type = UPDATE_<%= name.toUpperCase() %>;
+
+    constructor(public payload: <%= classify(name) %>) {}
+  }
+}
