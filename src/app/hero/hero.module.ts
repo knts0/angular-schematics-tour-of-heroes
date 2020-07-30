1a9b26fd943d25022ea837e60d55a2d9d6f791d9
knts0 <shimmer22.0204@gmail.com>
2020-07-31 00:03:21 +0900

    add: child module file

---
+++ src/app/hero/hero.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+
+import { NgxsModule } from '@ngxs/store';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { HeroState } from './hero.state';
+
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
+import { HeroesComponent }      from './heroes/heroes.component';
+import { HeroSearchComponent }  from './hero-search/hero-search.component';
+import { MessagesComponent }    from './messages/messages.component';
+
+
+const VIEW_COMPONENTS = [
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    HeroSearchComponent,
+    MessagesComponent,
+ ]
+
+@NgModule({
+  imports: [
+    NgxsModule.forFeature([
+      HeroState
+    ]),
+    NgxsLoggerPluginModule.forRoot(),
+  ],
+  exports: [ VIEW_COMPONENTS ],
+  declarations: [ VIEW_COMPONENTS ],
+})
+export class HeroModule { }
