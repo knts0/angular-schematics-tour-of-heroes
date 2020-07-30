2dbc4714f257cca23bbae774cafe7758a4adb6ff
knts0 <shimmer22.0204@gmail.com>
2020-07-31 02:21:54 +0900

    fix child views.module.ts(in memory db service setting)

--- schematics/src/hero/template-files/__name__/views.module.ts
+++ schematics/src/hero/template-files/__name__/views.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { NgxsModule } from '@ngxs/store';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { <%= classify(name) %>State } from './<%= name %>.state';
 
-import { DashboardComponent }   from './dashboard/dashboard.component';
-import { <%= classify(name) %>DetailComponent }  from './<%= name %>-detail/<%= name %>-detail.component';
-import { <%= classify(namePlural) %>Component }      from './<%= namePlural %>/<%= namePlural %>.component';
-import { <%= classify(name) %>SearchComponent }  from './<%= name %>-search/<%= name %>-search.component';
-import { MessagesComponent }    from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { <%= classify(name) %>DetailComponent } from './<%= name %>-detail/<%= name %>-detail.component';
+import { <%= classify(namePlural) %>Component } from './<%= namePlural %>/<%= namePlural %>.component';
+import { <%= classify(name) %>SearchComponent } from './<%= name %>-search/<%= name %>-search.component';
+import { MessagesComponent } from './messages/messages.component';
 
 
 const VIEW_COMPONENTS = [
@@ -24,7 +26,9 @@ const VIEW_COMPONENTS = [
     NgxsModule.forFeature([
       <%= classify(name) %>State
     ]),
-    NgxsLoggerPluginModule.forRoot(),
+    HttpClientInMemoryWebApiModule.forFeature(
+      InMemoryDataService, { dataEncapsulation: false }
+    ),
   ],
   exports: [ VIEW_COMPONENTS ],
   declarations: [ VIEW_COMPONENTS ],
--- src/app/hero/views.module.ts
+++ src/app/hero/views.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { NgxsModule } from '@ngxs/store';
-import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { HeroState } from './hero.state';
 
-import { DashboardComponent }   from './dashboard/dashboard.component';
-import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
-import { HeroesComponent }      from './heroes/heroes.component';
-import { HeroSearchComponent }  from './hero-search/hero-search.component';
-import { MessagesComponent }    from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { MessagesComponent } from './messages/messages.component';
 
 
 const VIEW_COMPONENTS = [
@@ -24,7 +25,9 @@ const VIEW_COMPONENTS = [
     NgxsModule.forFeature([
       HeroState
     ]),
-    NgxsLoggerPluginModule.forRoot(),
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    ),
   ],
   exports: VIEW_COMPONENTS,
   declarations: VIEW_COMPONENTS,
