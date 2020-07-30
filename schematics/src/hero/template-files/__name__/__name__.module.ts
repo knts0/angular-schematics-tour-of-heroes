ca41f1ad8333099318cf3b502cfd3ebf8801a3b9
knts0 <shimmer22.0204@gmail.com>
2020-07-31 00:05:21 +0900

    add module file to template files

---
+++ schematics/src/hero/template-files/__name__/__name__.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+
+import { NgxsModule } from '@ngxs/store';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { <%= classify(name) %>State } from './<%= name %>.state';
+
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { <%= classify(name) %>DetailComponent }  from './<%= name %>-detail/<%= name %>-detail.component';
+import { <%= classify(namePlural) %>Component }      from './<%= namePlural %>/<%= namePlural %>.component';
+import { <%= classify(name) %>SearchComponent }  from './<%= name %>-search/<%= name %>-search.component';
+import { MessagesComponent }    from './messages/messages.component';
+
+
+const VIEW_COMPONENTS = [
+    DashboardComponent,
+    <%= classify(name) %>DetailComponent,
+    <%= classify(namePlural) %>Component,
+    <%= classify(name) %>SearchComponent,
+    MessagesComponent,
+ ]
+
+@NgModule({
+  imports: [
+    NgxsModule.forFeature([
+      <%= classify(name) %>State
+    ]),
+    NgxsLoggerPluginModule.forRoot(),
+  ],
+  exports: [ VIEW_COMPONENTS ],
+  declarations: [ VIEW_COMPONENTS ],
+})
+export class <%= classify(name) %>Module { }
