7548993b971df352129c4ab319ec0a6af763d8a0
knts0 <shimmer22.0204@gmail.com>
2020-07-31 02:20:44 +0900

    fix route directory files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { DashboardComponent }   from './hero/dashboard/dashboard.component';
-import { HeroesComponent }      from './hero/heroes/heroes.component';
-import { HeroDetailComponent }  from './hero/hero-detail/hero-detail.component';
+import { DashboardComponent }   from './hero';
+import { HeroesComponent }      from './hero';
+import { HeroDetailComponent }  from './hero';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule }    from '@angular/forms';
-import { HttpClientModule }    from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { NgxsModule } from '@ngxs/store';
+import { NgxsRouterPluginModule } from '@ngxs/router-plugin'
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 
 import * as hero from './hero'
@@ -18,12 +17,9 @@ const MODULES = [
   AppRoutingModule,
   HttpClientModule,
   NgxsModule.forRoot([]),
+  NgxsRouterPluginModule.forRoot(),
   NgxsLoggerPluginModule.forRoot(),
   hero.ViewsModule,
-
-  HttpClientInMemoryWebApiModule.forRoot(
-    InMemoryDataService, { dataEncapsulation: false }
-  ),
 ]
 
 @NgModule({
@@ -31,6 +27,6 @@ const MODULES = [
     AppComponent,
   ],
   imports: MODULES,
-  bootstrap: [ AppComponent ]
+  bootstrap: [ AppComponent ],
 })
 export class AppModule { }
