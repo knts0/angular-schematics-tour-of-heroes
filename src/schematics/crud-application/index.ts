48ead3e633661c02ebc48a49d429f8ab68a0a071
knts0 <shimmer22.0204@gmail.com>
2020-08-03 00:27:25 +0900

    add schema.json and refactor index.ts

--- src/schematics/crud-application/index.ts
+++ src/schematics/crud-application/index.ts
@@ -1,43 +1,116 @@
 import {
   Rule,
   SchematicContext,
+  SchematicsException,
   Tree,
   apply,
   branchAndMerge,
+  chain,
   mergeWith,
   move,
   template,
   url,
 } from '@angular-devkit/schematics';
+
 import {
-  basename,
   normalize,
   strings,
   Path,
 } from '@angular-devkit/core';
+
+import { addImportToModule } from '@schematics/angular/utility/ast-utils';
+
+import {
+  buildRelativePath,
+  findModuleFromOptions,
+} from '@schematics/angular/utility/find-module';
+
+import { InsertChange } from '@schematics/angular/utility/change';
+import * as ts from '@schematics/angular/third_party/github.com/Microsoft/TypeScript/lib/typescript';
+import { Schema as ComponentOptions } from '@schematics/angular/component/schema';
+
+function readIntoSourceFile(host: Tree, modulePath: string): ts.SourceFile {
+  const text = host.read(modulePath);
+  if (text === null) {
+    throw new SchematicsException(`File ${modulePath} does not exist.`);
+  }
+  const sourceText = text.toString('utf-8');
+
+  return ts.createSourceFile(modulePath, sourceText, ts.ScriptTarget.Latest, true);
+}
+
+function addImportToNgModule(options: ComponentOptions): Rule {
+  return (host: Tree) => {
+    if (!options.module) {
+      return host;
+    }
+
+    // 変更を加えたいNgModuleのパス
+    const modulePath = options.module;
+    // NgModuleファイルの内容を表す、ts.SourceFile型のオブジェクト
+    const source = readIntoSourceFile(host, modulePath);
+
+    // 子モジュールのパス
+    const childModulePath = `/${options.path}/`
+                          + strings.dasherize(options.name) + '/'
+                          + strings.dasherize(options.name)
+                          + '.'
+                          + 'module';
+
+    // modulePathからchildModulePathへの相対パス
+    const relativePath = buildRelativePath(modulePath, childModulePath);
+
+    // SampleModule　のような子モジュールのクラス名
+    const classifiedName = strings.classify(options.name) + 'Module';
+
+    // 子モジュールのimports宣言を追加
+    const declarationChanges = addImportToModule(source,
+                                                 modulePath,
+                                                 classifiedName,
+                                                 relativePath);
+
+    const declarationRecorder = host.beginUpdate(modulePath);
+    for (const change of declarationChanges) {
+      if (change instanceof InsertChange) {
+        declarationRecorder.insertLeft(change.pos, change.toAdd);
+      }
+    }
+    host.commitUpdate(declarationRecorder);
+
+    return host;
+  };
+}
   
 export function crudSchematics(options: any): Rule {
-  return (_: Tree, _context: SchematicContext) => {
+  return (host: Tree, _context: SchematicContext) => {
     if (options.path === undefined) {
       options.path = 'src/app';
     }
+
+    // 生成したコードに関する宣言を追加するNgModuleのパスを取得
+    options.module = findModuleFromOptions(host, options);
     
-    const nameWithoutPath = basename(normalize(options.name))
+    // コードを生成するパスの指定
     const pathToCreate: Path = normalize(options.path as string)
-    options.name = nameWithoutPath
     options.path = pathToCreate
 
-    return branchAndMerge(
-      mergeWith(
-        apply(url('./template-files'), [
-          template({
-            ...strings,
-            ...options,
-          }),
-          move(pathToCreate)
-        ])
+    return chain([
+      // 生成したコードに関する宣言をNgModuleに追加する
+      addImportToNgModule(options),
+
+      // コードの生成
+      branchAndMerge(
+        mergeWith(
+          apply(url('./template-files'), [
+            template({
+              ...strings,
+              ...options,
+            }),
+            move(pathToCreate)
+          ])
+        )
       )
-    )
+    ])
   };
 }
   
