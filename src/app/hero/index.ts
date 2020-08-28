a60f407e231ceebc3291db44a8c6315803c05c42
kana otawara <kana.otawara@nextbeat.net>
2020-08-28 18:02:36 +0900

    delete hero files

--- src/app/hero/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './hero.module';
-export * from './hero-detail/hero-detail.component';
-export * from './hero-search/hero-search.component';
-export * from './heroes/heroes.component';
-export * from './hero';
